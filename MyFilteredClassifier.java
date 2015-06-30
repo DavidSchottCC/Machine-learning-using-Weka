9ef2c6e5ca25690cef2f79512b2f065b03aba74b
DavidSchottCC <david.schott@codecentric.de>
2015-06-30 16:10:23 +0200

    adapted version

--- MyFilteredClassifier.java
+++ MyFilteredClassifier.java
@@ -1,32 +1,26 @@
+package de.codecentric.docnitio.experimenter.helloworld.helloweka;
+
 /**
  * A Java class that implements a simple text classifier, based on WEKA.
  * To be used with MyFilteredLearner.java.
  * WEKA is available at: http://www.cs.waikato.ac.nz/ml/weka/
- * Copyright (C) 2013 Jose Maria Gomez Hidalgo - http://www.esp.uem.es/jmgomez
- *
- * This program is free software: you can redistribute it and/or modify
- * it for any purpose.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
- 
+
 import weka.core.*;
-import weka.core.FastVector;
 import weka.classifiers.meta.FilteredClassifier;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.io.*;
 
 /**
- * This class implements a simple text classifier in Java using WEKA.
- * It loads a file with the text to classify, and the model that has been
- * learnt with MyFilteredLearner.java.
- * @author Jose Maria Gomez Hidalgo - http://www.esp.uem.es/jmgomez
+ * This class implements a simple text classifier in Java using WEKA. It loads a
+ * file with the text to classify, and the model that has been learnt with
+ * MyFilteredLearner.java.
+ * 
  * @see MyFilteredLearner
  */
- public class MyFilteredClassifier {
+public class MyFilteredClassifier {
 
 	/**
 	 * String that stores the text to classify
@@ -40,103 +34,111 @@ import java.io.*;
 	 * Object that stores the classifier.
 	 */
 	FilteredClassifier classifier;
-		
+
 	/**
 	 * This method loads the text to be classified.
-	 * @param fileName The name of the file that stores the text.
+	 * 
+	 * @param filePath
+	 *            The name of the file that stores the text.
 	 */
-	public void load(String fileName) {
+	public void loadData(String filePath) {
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			BufferedReader reader = new BufferedReader(new FileReader(filePath));
 			String line;
 			text = "";
 			while ((line = reader.readLine()) != null) {
-                text = text + " " + line;
-            }
-			System.out.println("===== Loaded text data: " + fileName + " =====");
+				text = text + " " + line;
+			}
+			System.out
+					.println("===== Loaded text data: " + filePath + " =====");
 			reader.close();
 			System.out.println(text);
-		}
-		catch (IOException e) {
-			System.out.println("Problem found when reading: " + fileName);
+		} catch (IOException e) {
+			System.out.println("Problem found when reading: " + filePath);
 		}
 	}
-			
+
 	/**
 	 * This method loads the model to be used as classifier.
-	 * @param fileName The name of the file that stores the text.
+	 * 
+	 * @param modelPath
+	 *            The path of the file that stores the classifier.
 	 */
-	public void loadModel(String fileName) {
+	public void loadModel(String modelPath) {
 		try {
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName));
-            Object tmp = in.readObject();
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(
+					modelPath));
+			Object tmp = in.readObject();
 			classifier = (FilteredClassifier) tmp;
-            in.close();
- 			System.out.println("===== Loaded model: " + fileName + " =====");
-       } 
-		catch (Exception e) {
-			// Given the cast, a ClassNotFoundException must be caught along with the IOException
-			System.out.println("Problem found when reading: " + fileName);
+			in.close();
+			System.out.println("===== Loaded model: " + modelPath + " =====");
+		} catch (Exception e) {
+			// Given the cast, a ClassNotFoundException must be caught along
+			// with the IOException
+			System.out.println("Problem found when reading: " + modelPath);
 		}
 	}
-	
+
 	/**
-	 * This method creates the instance to be classified, from the text that has been read.
+	 * This method creates the instance to be classified, from the text that has
+	 * been read.
 	 */
-	public void makeInstance() {
-		// Create the attributes, class and text
-		FastVector fvNominalVal = new FastVector(2);
+	@SuppressWarnings("deprecation")
+	public void makeInstances() {
+		// Create the 2 attributes, class = {spam, ham} and text = string
+		FastVector<String> fvNominalVal = new FastVector<String>(2);
 		fvNominalVal.addElement("spam");
 		fvNominalVal.addElement("ham");
-		Attribute attribute1 = new Attribute("class", fvNominalVal);
-		Attribute attribute2 = new Attribute("text",(FastVector) null);
-		// Create list of instances with one element
-		FastVector fvWekaAttributes = new FastVector(2);
+		Attribute attribute1 = new Attribute("class", fvNominalVal); // @attribute spamclass {spam,ham}
+		Attribute attribute2 = new Attribute("text", (FastVector<String>) null); // @attribute text String
+		// Create list of instances with capacity 1
+		FastVector<Attribute> fvWekaAttributes = new FastVector<Attribute>(2);
 		fvWekaAttributes.addElement(attribute1);
 		fvWekaAttributes.addElement(attribute2);
-		instances = new Instances("Test relation", fvWekaAttributes, 1);           
+		instances = new Instances("Test relation", fvWekaAttributes, 1);
 		// Set class index
 		instances.setClassIndex(0);
-		// Create and add the instance
+		// Create and add the instance to instances
 		DenseInstance instance = new DenseInstance(2);
-		instance.setValue(attribute2, text);
-		// Another way to do it:
-		// instance.setValue((Attribute)fvWekaAttributes.elementAt(1), text);
+		instance.setValue((Attribute) fvWekaAttributes.elementAt(1), text);
 		instances.add(instance);
- 		System.out.println("===== Instance created with reference dataset =====");
+		System.out
+				.println("===== Instance created with reference dataset =====");
 		System.out.println(instances);
 	}
-	
+
 	/**
-	 * This method performs the classification of the instance.
-	 * Output is done at the command-line.
+	 * This method performs the classification of the instance. Output is done
+	 * at the command-line.
 	 */
 	public void classify() {
 		try {
 			double pred = classifier.classifyInstance(instances.instance(0));
+			double[] preds = classifier.distributionForInstance(instances
+					.instance(0));
 			System.out.println("===== Classified instance =====");
-			System.out.println("Class predicted: " + instances.classAttribute().value((int) pred));
-		}
-		catch (Exception e) {
+			System.out.println("Class predicted: "
+					+ instances.classAttribute().value((int) pred));
+			System.out.println("Ham probability: " + preds[1]);
+			System.out.println("Spam probability: " + preds[0]);
+		} catch (Exception e) {
 			System.out.println("Problem found when classifying the text");
-		}		
+		}
 	}
-	
+
 	/**
 	 * Main method. It is an example of the usage of this class.
-	 * @param args Command-line arguments: fileData and fileModel.
+	 * 
+	 * @param args
+	 *            Command-line arguments: fileData and fileModel.
 	 */
-	public static void main (String[] args) {
-	
+	public static void main(String[] args) {
+		String dirPath = "C:\\Users\\DavidCC\\Desktop\\Weka Beispiel\\tmweka\\FilteredClassifier\\";
 		MyFilteredClassifier classifier;
-		if (args.length < 2)
-			System.out.println("Usage: java MyClassifier <fileData> <fileModel>");
-		else {
-			classifier = new MyFilteredClassifier();
-			classifier.load(args[0]);
-			classifier.loadModel(args[1]);
-			classifier.makeInstance();
-			classifier.classify();
-		}
+		classifier = new MyFilteredClassifier();
+		classifier.loadData(dirPath + "smstest.txt");
+		classifier.loadModel(dirPath + "trained-classifier.obj");
+		classifier.makeInstances();
+		classifier.classify();
 	}
-}	
+}
--- MyFilteredLearner.java
+++ MyFilteredLearner.java
@@ -1,32 +1,29 @@
+package de.codecentric.docnitio.experimenter.helloworld.helloweka;
+
 /**
  * A Java class that implements a simple text learner, based on WEKA.
  * To be used with MyFilteredClassifier.java.
  * WEKA is available at: http://www.cs.waikato.ac.nz/ml/weka/
- * Copyright (C) 2013 Jose Maria Gomez Hidalgo - http://www.esp.uem.es/jmgomez
- *
- * This program is free software: you can redistribute it and/or modify
- * it for any purpose.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 import weka.classifiers.Evaluation;
+
 import java.util.Random;
+
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.meta.FilteredClassifier;
+import weka.core.converters.ArffSaver;
 import weka.core.converters.ArffLoader.ArffReader;
+
 import java.io.*;
 
 /**
- * This class implements a simple text learner in Java using WEKA.
- * It loads a text dataset written in ARFF format, evaluates a classifier on it,
- * and saves the learnt model for further use.
- * @author Jose Maria Gomez Hidalgo - http://www.esp.uem.es/jmgomez
+ * This class implements a simple text learner in Java using WEKA. It loads a
+ * text dataset written in ARFF format, evaluates a classifier on it, and saves
+ * the learnt model for further use.
  * @see MyFilteredClassifier
  */
 public class MyFilteredLearner {
@@ -43,104 +40,148 @@ public class MyFilteredLearner {
 	 * Object that stores the classifier
 	 */
 	FilteredClassifier classifier;
-		
+
 	/**
-	 * This method loads a dataset in ARFF format. If the file does not exist, or
-	 * it has a wrong format, the attribute trainData is null.
-	 * @param fileName The name of the file that stores the dataset.
+	 * This method loads a dataset in ARFF format. If the file does not exist,
+	 * or it has a wrong format, the attribute trainData is null.
+	 * 
+	 * @param fileName
+	 *            The name of the file that stores the dataset.
 	 */
-	public void loadDataset(String fileName) {
+	public void load(String fileName) {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(fileName));
 			ArffReader arff = new ArffReader(reader);
 			trainData = arff.getData();
 			System.out.println("===== Loaded dataset: " + fileName + " =====");
 			reader.close();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("Problem found when reading: " + fileName);
 		}
 	}
-	
+
 	/**
-	 * This method evaluates the classifier. As recommended by WEKA documentation,
-	 * the classifier is defined but not trained yet. Evaluation of previously
-	 * trained classifiers can lead to unexpected results.
+	 * This method evaluates the classifier. As recommended by WEKA
+	 * documentation, the classifier is defined but not trained yet. Evaluation
+	 * of previously trained classifiers can lead to unexpected results.
 	 */
 	public void evaluate() {
 		try {
-			trainData.setClassIndex(0);
-			filter = new StringToWordVector();
-			filter.setAttributeIndices("last");
+			// Our training data is tuple of form (class, text)
+			trainData.setClassIndex(0); // Class of dataset is first attribute
+			// Classifier
 			classifier = new FilteredClassifier();
 			classifier.setFilter(filter);
 			classifier.setClassifier(new NaiveBayes());
 			Evaluation eval = new Evaluation(trainData);
-			eval.crossValidateModel(classifier, trainData, 4, new Random(1));
+			eval.crossValidateModel(classifier, trainData, 10, new Random(1));
 			System.out.println(eval.toSummaryString());
 			System.out.println(eval.toClassDetailsString());
-			System.out.println("===== Evaluating on filtered (training) dataset done =====");
-		}
-		catch (Exception e) {
-			System.out.println("Problem found when evaluating");
+			System.out
+					.println("===== Evaluating on filtered (training) dataset done =====");
+		} catch (Exception e) {
+			System.out.println("Problem found when evaluating:\t"
+					+ e.getMessage());
 		}
 	}
-	
+
 	/**
 	 * This method trains the classifier on the loaded dataset.
 	 */
 	public void learn() {
 		try {
 			trainData.setClassIndex(0);
-			filter = new StringToWordVector();
-			filter.setAttributeIndices("last");
 			classifier = new FilteredClassifier();
 			classifier.setFilter(filter);
 			classifier.setClassifier(new NaiveBayes());
 			classifier.buildClassifier(trainData);
-			// Uncomment to see the classifier
 			// System.out.println(classifier);
-			System.out.println("===== Training on filtered (training) dataset done =====");
-		}
-		catch (Exception e) {
-			System.out.println("Problem found when training");
+			System.out
+					.println("===== Training on filtered (training) dataset done =====");
+		} catch (Exception e) {
+			System.out.println("Problem found when training:\t"
+					+ e.getMessage());
 		}
 	}
-	
+
 	/**
-	 * This method saves the trained model into a file. This is done by
-	 * simple serialization of the classifier object.
-	 * @param fileName The name of the file that will store the trained model.
+	 * This method saves the trained model into a file. This is done by simple
+	 * serialization of the classifier object.
+	 * 
+	 * @param filePath The path of the file that will store the trained model.
+	 *            
 	 */
-	public void saveModel(String fileName) {
+	public void saveModel(String filePath) {
 		try {
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName));
-            out.writeObject(classifier);
-            out.close();
- 			System.out.println("===== Saved model: " + fileName + " =====");
-        } 
-		catch (IOException e) {
-			System.out.println("Problem found when writing: " + fileName);
+			ObjectOutputStream out = new ObjectOutputStream(
+					new FileOutputStream(filePath));
+			out.writeObject(classifier);
+			out.close();
+			System.out.println("===== Saved model: " + filePath + " =====");
+		} catch (IOException e) {
+			System.out.println("Problem found when writing: " + filePath);
 		}
 	}
-	
+
+	/**
+	 * This method sets the pre-processing filters to use for preparing the
+	 * dataset dataset.
+	 */
+	public void preProcess() {
+		try {
+			filter = new StringToWordVector();
+			filter.setInputFormat(trainData);
+			filter.setAttributeIndices("last");
+			// filter.setUseStoplist(true);
+			// filter.setIDFTransform(true);
+			System.out.println("===== Filter of (training) dataset set =====");
+		} catch (Exception e) {
+			System.out.println("Problem found when pre-processing data "
+					+ e.getMessage());
+		}
+	}
+
+	/**
+	 * Applies the preprocessing filters to the dataset and writes an ARFF-file
+	 * to a given path.
+	 * 
+	 * @param savePath
+	 *            the full path of the ARFF-file
+	 */
+	public void saveFilteredData(String savePath) {
+		try {
+			Instances trainDataFiltered = Filter.useFilter(trainData, filter);
+			// if savePath has been set
+			if (!savePath.isEmpty()) {
+				ArffSaver saver = new ArffSaver();
+				saver.setInstances(trainDataFiltered);
+				saver.setFile(new File(savePath));
+				saver.writeBatch();
+				System.out
+						.println("===== Saved filtered (training) dataset to: "
+								+ savePath + " =====");
+			}
+		} catch (Exception e) {
+			System.out.println("saving filtered data to Path:\n" + savePath
+					+ "\n" + e.getMessage());
+		}
+	}
+
 	/**
 	 * Main method. It is an example of the usage of this class.
-	 * @param args Command-line arguments: fileData and fileModel.
+	 * 
+	 * @param args
+	 *            Command-line arguments: fileData-Input and fileModel-Output
+	 *            object.
 	 */
-	public static void main (String[] args) {
-	
+	public static void main(String[] args) throws Exception {
 		MyFilteredLearner learner;
-		if (args.length < 2)
-			System.out.println("Usage: java MyLearner <fileData> <fileModel>");
-		else {
-			learner = new MyFilteredLearner();
-			learner.loadDataset(args[0]);
-			// Evaluation mus be done before training
-			// More info in: http://weka.wikispaces.com/Use+WEKA+in+your+Java+code
-			learner.evaluate();
-			learner.learn();
-			learner.saveModel(args[1]);
-		}
+		String datasetPath = "Users/user1/Documents/learner/";
+		learner = new MyFilteredLearner();
+		learner.load(datasetPath + "smsspam.big.arff");
+		learner.preProcess();
+		learner.evaluate();
+		learner.learn();
+		learner.saveModel(datasetPath + "trained-classifier.obj");
 	}
-}	
+}
